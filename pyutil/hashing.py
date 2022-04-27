bccc0a9fc06908ceac4457c36896b9b761fe8d90
stefano <stefano.katsoras@gmail.com>
2022-04-27 14:08:08 -0400

    debug hashing

--- pyutil/hashing.py
+++ pyutil/hashing.py
@@ -8,6 +8,8 @@ logger = logging.getLogger(__name__)
 def hash_item(i):
     """Hash a python object by pickling and then applying MD5 to resulting bytes"""
     if isinstance(i, dict):
+        hash = hash_dict(i)
+        print(f"Hashing item: {i} --> {hash}")
         return hash_dict(i)
     try:
         hash = hashlib.md5(pickle.dumps(i)).hexdigest()
@@ -17,12 +19,16 @@ def hash_item(i):
     except AttributeError:
         logger.warning(f"Unable to hash the objects class, using hash of class name instead")
         hash = hashlib.md5(pickle.dumps(i.__class__.__name__)).hexdigest()
-    logger.info(f"Hashing item: {i} --> {hash}")
+    print(f"Hashing item: {i} --> {hash}")
     return hash
 
 
 def hash_dict(d: dict) -> str:
     """Hash dict in order-agnostic manner by ordering keys and hashes of values"""
+
+    if len(d) == 0:
+        # Empty dict
+        return "0"
 
     # order keys and hash result
     key_hash = hash_item(list(sorted(d.keys())))
