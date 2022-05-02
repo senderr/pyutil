b19c2fef2b704de6d9472bb5a26eaff061c27a60
stefano <stefano.katsoras@gmail.com>
2022-05-01 20:09:21 -0400

    import all modules in __init__

--- pyutil/__init__.py
+++ pyutil/__init__.py
@@ -3,4 +3,8 @@ import logging
 root_logger = logging.getLogger("pyutil")
 root_logger.setLevel(logging.INFO)
 root_logger.addHandler(logging.StreamHandler())
-root_logger.handlers[0].setFormatter(logging.Formatter("{levelname:<8} {name:>30}{funcName:>35}:{lineno:<4} {message}", style="{"))
+root_logger.handlers[0].setFormatter(
+    logging.Formatter("{levelname:<8} {name:>30}{funcName:>35}:{lineno:<4} {message}", style="{")
+)
+
+from . import cache, dicts, enums, hashing, io, misc, profiling
