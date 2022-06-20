73588a1b50dc3d767213d1bb485ea706e0f64a08
stefano <stefano.katsoras@gmail.com>
2022-06-20 17:20:18 -0400

    add docstring to get_full_kwargs

--- pyutil/misc.py
+++ pyutil/misc.py
@@ -2,7 +2,15 @@ import inspect
 
 
 def get_full_kwargs(func, kwargs):
-    """Return `func`'s fallback kwargs + explicit kwargs combined"""
+    """Return `func`'s fallback kwargs + explicit kwargs combined
+
+    Example:
+    >>> def a(b=1, c=2):
+    >>>     return b + c
+
+    >>> get_full_kwargs(a, {})
+    {'b': 1, 'c': 2}
+    """
 
     default_kwargs = {
         k: v.default for k, v in inspect.signature(func).parameters.items() if v.default is not inspect.Parameter.empty
