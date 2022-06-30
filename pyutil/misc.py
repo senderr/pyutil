f767d8a83385c01a22f18e3d99e6fa43f3d4ee38
stefano <stefano.katsoras@gmail.com>
2022-06-29 23:12:16 -0400

    get_named_args

--- pyutil/misc.py
+++ pyutil/misc.py
@@ -1,7 +1,6 @@
 import inspect
 
-
-def get_full_kwargs(func, kwargs):
+def get_full_kwargs(func, kwargs) -> dict:
     """Return `func`'s fallback kwargs + explicit kwargs combined
 
     Example:
@@ -17,3 +16,17 @@ def get_full_kwargs(func, kwargs):
     }
     default_kwargs.update(kwargs)
     return default_kwargs
+
+def get_named_args(func, args) -> dict:
+    """Return `func`'s arguments as a dictionary with keys as the argument names as defined in `func`
+    
+    Example:
+    >>> def a(a, b, c):
+    >>>     return a + b + c
+
+    >>> get_named_args(a, (5, 6, 7))
+    {"a": 5, "b": 6, "c": 7}
+    """
+    arg_names = [k for k, v in inspect.signature(func).parameters.items() if v.kind == v.POSITIONAL_OR_KEYWORD and v.default == v.empty]
+    return dict(zip(arg_names, args))
+    
