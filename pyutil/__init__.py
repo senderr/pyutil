c5e9f4a68c9fabddf34ecf86daa77406c24e9d41
stefano <stefano.katsoras@gmail.com>
2022-07-18 00:07:33 -0400

    update logger

--- pyutil/__init__.py
+++ pyutil/__init__.py
@@ -4,7 +4,11 @@ root_logger = logging.getLogger("pyutil")
 root_logger.setLevel(logging.INFO)
 root_logger.addHandler(logging.StreamHandler())
 root_logger.handlers[0].setFormatter(
-    logging.Formatter("{levelname:<8} {name:>30}{funcName:>35}:{lineno:<4} {message}", style="{")
+    logging.Formatter(
+        "{asctime}.{msecs:03.0f} {levelname:<8} {name:<50}{funcName:>35}:{lineno:<4} {message}",
+        style="{",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
 )
 
 from . import cache, dicts, enums, hashing, io, misc, profiling
